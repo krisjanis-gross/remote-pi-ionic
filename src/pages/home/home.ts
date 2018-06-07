0431eb23ccb12f1f4858941a5cd10b85b2f9756d
Krisjanis Gross <krishinsh@gmail.com>
2018-06-07 14:30:26 +0300

    initial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,13 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  status_message:string;
+  warmth: number;
+  delta_t: number;
   constructor(public navCtrl: NavController) {
-
+      this.status_message = "Connected OK/Connecting/Can not reach/";
+      this.warmth = 24;
+      this.delta_t = 5;
   }
 
 }
